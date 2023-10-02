84720fdd256de9632db02b70592c59df476e1ea6
Rahul Kumar <rahulkumar981417@gamil.com>
2023-10-02 11:39:29 +0530

    rkcode

---
+++ script/validation.js
@@ -0,0 +1,17 @@
+var data ={
+    name: "admin",
+    password:"admin"
+}
+
+function validate(){
+    var name = document.getElementById("uname").value;
+    var pwd = document.getElementById("pwd").value;
+    var msg = document.getElementById("msg");
+    var button = document.querySelector("button");
+    if(name==data.name && pwd==data.password){
+        window.location.href ="index.html";
+    }else{
+        msg.className="text-danger";
+        msg.innerHTML="Invalid credentials";
+    }
+}
